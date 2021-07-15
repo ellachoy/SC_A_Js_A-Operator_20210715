1cb036d68ef6f6c275b119637e744d942bec40ce
Ella Choy <egal21de@gmail.com>
2021-07-15 17:29:54 +0200

    finit

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+
+// Multiplikation geht vor und so wird 5 * 10 zuerst durchgeführt.
+
+/*let score = 5 + 5 * 10;
+ console.log('Ergebnis: ' + score);*/
+
+//  Du kannst Klammern() hinzufügen, um die Reihenfolge der Prioritäten zu ändern.
+
+/*let score = (5 + 5) * 10;
+console.log('Ergebnis: ' + score);*/
+
+
+// Du kannst Werte auf Variablen addieren.
+
+let score = 0;
+score = score + 10;
+console.log('Ergebnis: ' + score);
+
+score += 10;
+console.log('Ergebnis: ' + score);
+
+// aufgabe
+
+let zahl = 1;
+// zahl = zahl + 1;
+zahl += 1;
+console.log("ergebnis:" + zahl)
+
+
+zahl ++
+console.log("increment:" + zahl)
+
+zahl --
+console.log("dekrement:" +zahl)
+
+
